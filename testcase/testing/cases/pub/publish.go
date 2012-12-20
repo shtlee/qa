71eb8e6d7410f74c6919ed671f01dcda6440df29
fangdong <fangdong@pipul.org>
2012-12-20 14:19:02 +0800

    update

--- testcase/testing/cases/pub/publish.go
+++ testcase/testing/cases/pub/publish.go
@@ -1,21 +1,37 @@
 package pub
 
 import (
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+	"crypto/sha1"
+	"encoding/hex"
+	"net/http"
+	"math/rand"
 	"path/filepath"
+	"qbox.us/errors"
 	"qbox.us/cc/config"
 	da "qbox.me/auth/digest"
 	"qbox.me/api"
+	"qbox.me/api/rs"
 	"qbox.me/api/pub"
 )
 
 type Pub struct {
-	Name               string
-	Bucket             string
-	Key                string
-	DataFile           string
-	NormalDomainRegexp string
+	Name               string  `json:"name"`
+	Bucket             string  `json:"bucket"`
+	Key                string  `json:"key"`
+	DataFile           string `json:"data_file"`
+	DataSha1 string `json:"data_sha1"`
+	dataType string
+	Domain string `json:"domain"`
+	DomainIp string `json:"domain_ip"`
+	isNormalDomain bool
+	NormalDomainRegexp string `json:"normal_domain_regexp"`
 
-	Conn *pub.Service
+	rsCli *rs.Service
+	pubCli *pub.Service
 	Env api.Env
 }
 
@@ -28,12 +44,142 @@ func (p *Pub) Init(conf, env, path string) (err error) {
 		return
 	}
 	dt := da.NewTransport(p.Env.AccessKey, p.Env.SecretKey, nil)
-	p.Conn, err = pub.New(p.Env.Hosts["pub"], p.Env.Ips["pub"], dt)
+	p.rsCli, err = rs.New(p.Env.Hosts, p.Env.Ips, dt)
+	if err != nil {
+		err = errors.Info(err, "Pub init failed")
+		return
+	}
+	p.pubCli, err = pub.New(p.Env.Hosts["pub"], p.Env.Ips["pub"], dt)
+	if err != nil {
+		err = errors.Info(err, "Pub init failed")
+		return
+	}
 	p.DataFile = filepath.Join(path, p.DataFile)
+	domainRegexp, err := regexp.Compile(p.NormalDomainRegexp)
+	if err != nil {
+		err = errors.Info(err, "Pub init failed")
+		return
+	}
+	p.isNormalDomain = domainRegexp.Match([]byte(p.Domain))
+
+	return
+}
+
+func (p *Pub) doTestUpload() (msg string, err error) {
+
+	p.dataType = "application/qbox-mon"
+	entryName := p.Bucket + ":" + p.Key
+	f, err := os.Open(p.DataFile)
+	if err != nil {
+		err = errors.Info(err, "Upload failed: ", p.DataFile)
+		return
+	}
+	defer f.Close()
+	fi, _ := f.Stat()
+	_, _, err = p.rsCli.Put(entryName, p.dataType, f, fi.Size())
+	if err != nil {
+		err = errors.Info(err, "upload failed:", entryName)
+	}
 	return
 }
 
 func (p *Pub) doTestPublish() (msg string, err error) {
 
+	if p.isNormalDomain {
+		p.Domain = p.Domain + "/" + strconv.FormatInt(rand.Int63(), 10)
+	} else {
+		p.Domain = strconv.FormatInt(rand.Int63(), 10) + "." + p.Domain
+	}
+	if _, err = p.rsCli.Publish(p.Domain, p.Bucket); err != nil {
+		err = errors.Info(err, "Publish failed: ", p.Bucket, p.Domain)
+		return
+	}
+	return
+}
+
+func (p *Pub) doTestDownload() (msg string, err error) {
+
+	var (
+		url string
+	)
+	if p.isNormalDomain {
+		url = "http://" + p.Domain + "/" + p.Key
+	} else {
+		url = "http://" + p.DomainIp + "/" + p.Key
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		err = errors.Info(err, "Download failed:", url)
+		return
+	}
+	if !p.isNormalDomain {
+		req.Host = p.Domain
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		err = errors.Info(err, "Download failed:", url)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode/100 != 2 {
+		err = errors.New("download status code is not 20x!")
+		err = errors.Info(err, url, resp.StatusCode)
+		return
+	}
+	if ct, ok := resp.Header["Content-Type"]; !ok || len(ct) != 1 || ct[0] != p.dataType {
+		err = errors.New("Invalid content type")
+		return
+	}
+
+	h := sha1.New()
+	if _, err = io.Copy(h, resp.Body); err != nil {
+		err = errors.Info(err, "check sha1 failed")
+		return
+	}
+	if p.DataSha1 != hex.EncodeToString(h.Sum(nil)) {
+		err = errors.New("Invalid data sha1")
+		err = errors.Info(err, p.DataSha1, hex.EncodeToString(h.Sum(nil)))
+		return
+	}
+	
+	return
+}
+
+
+func (p *Pub) doTestUnpublish() (msg string, err error) {
+
+	if _, err = p.rsCli.Unpublish(p.Domain); err != nil {
+		err = errors.Info(err, "unpublish domain failed", p.Domain)
+		return
+	}
+	return
+}
+
+
+func (p *Pub) Test() (msg string, err error) {
+
+	log1, err := p.doTestUpload()
+	msg += log1
+	if err != nil {
+		return
+	}
+
+	log1, err = p.doTestPublish()
+	msg += log1
+	if err != nil {
+		return
+	}
+
+	log1, err = p.doTestDownload()
+	msg += log1
+	if err != nil {
+		return
+	}
+
+	log1, err = p.doTestUnpublish()
+	msg += log1
+	if err != nil {
+		return
+	}
 	return
 }
--- testcase/testing/interface.go
+++ testcase/testing/interface.go
@@ -4,25 +4,29 @@ import (
 	"./cases/example"
 	"./cases/fop"
 	"./cases/up"
+	"./cases/pub"
+)
+
+
+var (
+	Cases = map[string]func () Interface{
+		"example": func() Interface { return &example.Example{} },
+		"resumable_put": func() Interface { return &up.UpResuPut{} },
+		"fop_img_info":  func() Interface { return &fop.FopImgInfo{} },
+		"fop_img_view":  func() Interface { return &fop.FopImgOp{} },
+		"fop_img_mogr":  func() Interface { return &fop.FopImgOp{} },
+		//"fop_img_exif":   &fop.FopImgExif{},
+		"up_put": func() Interface { return &up.PutFile{} },
+		//"old_mon":   &Old{},
+		//"rs":          &Rs{},
+		//"rs_upload":   &RsUpload{},
+		"publish": func() Interface { return &pub.Pub{} },
+		//"shell":       &Shell{},
+		//"up":          &Up{},
+	}
 )
 
 type Interface interface {
 	Init(conf, env, path string) error
 	Test() (msg string, err error)
-}
-
-var Cases = map[string]Interface{
-	"example":       &example.Example{},
-	"resumable_put": &up.UpResuPut{},
-	"fop_img_info":  &fop.FopImgInfo{},
-	"fop_img_view":  &fop.FopImgOp{},
-	"fop_img_mogr":  &fop.FopImgOp{},
-	//"fop_img_exif"        :    &fop.FopImgExif{},
-	"up_put": &up.PutFile{},
-	/*"old_mon":   &Old{},
-	"rs":          &Rs{},
-	"rs_upload":   &RsUpload{},
-	"publish":     &Publish{},
-	"shell":       &Shell{},
-	"up":          &Up{},*/
 }
