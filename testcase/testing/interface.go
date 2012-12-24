868843cb30bed043fafb446cc5bc69d3297a8ed7
fangdong <fangdong@pipul.org>
2012-12-24 16:29:37 +0800

    add up.UpRPut to Interface

--- testcase/testing/interface.go
+++ testcase/testing/interface.go
@@ -12,11 +12,12 @@ var (
 	Cases = map[string]func () Interface{
 		"example": func() Interface { return &example.Example{} },
 		"resumable_put": func() Interface { return &up.UpPut{} },
+		"up_put": func() Interface { return &up.UptokenPut{} },
+		"up_put2": func() Interface { return &up.UpRPut{} },
 		"fop_img_info":  func() Interface { return &fop.FopImgInfo{} },
 		"fop_img_view":  func() Interface { return &fop.FopImgOp{} },
 		"fop_img_mogr":  func() Interface { return &fop.FopImgOp{} },
 		//"fop_img_exif":   &fop.FopImgExif{},
-		"up_put": func() Interface { return &up.UptokenPut{} },
 		//"old_mon":   &Old{},
 		//"rs":          &Rs{},
 		//"rs_upload":   &RsUpload{},
