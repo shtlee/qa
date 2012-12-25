c333c86c8e72f3277517a3557a9b15c11d8400d2
fangdong <fangdong@pipul.org>
2012-12-25 16:21:56 +0800

    update

--- testcase/testing/cases/pub/pub_image.go
+++ testcase/testing/cases/pub/pub_image.go
@@ -43,10 +43,20 @@ func (p *Pub) doTestImage() (msg string, err error) {
 
 	from := []string{p.FromDomain}
 	code, err := p.Pubcli.Image(p.Bucket, from, p.SrcHost, 0)
-	if err != nil {
-		err = errors.Info(err, "doTestImage failed", p.SrcHost, p.FromDomain, code)
+	if err != nil || code/100 != 2 {
+		if !err {
+			err = errors.New("doTestImage failed")
+		} 
+		err = errors.Info(err, code, p.SrcHost, p.FromDomain)
 		return
 	}
+	url := "http://" + p.Env.Hosts["io"] + "/" + p.SrcKey
+	r, err := httputil.DownloadEx(url, p.FromDomain)
+	if err != nil {
+		err = errors.Info(err, "doTestImage failed", url)
+		return
+	}
+	
 }
 
 
