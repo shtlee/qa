3055ebc36cb4c625cc1b4289e405826371767382
fangdong <fangdong@pipul.org>
2012-12-25 11:10:18 +0800

    update

--- testcase/testing/cases/pub/pub_image.go
+++ testcase/testing/cases/pub/pub_image.go
@@ -1,6 +1,8 @@
 package pub
 
 import (
+	"qbox.us/cc/config"
+	"qbox.us/errors"
 	"qbox.me/api"	
 	"qbox.me/api/pub"
 )
@@ -9,18 +11,37 @@ type PubImage struct {
 
 	Name string `json:"name"`
 	Bucket string `json:"bucket"`
-	Key string `json:"key"`
-	DataFile string `json:"data_file"`
-	DataSha1 string `json:"data_sha1"`
 	Domain string `json:"image_domain"`
-	ImageURL string `json:"source_image_url"`
-	
+	FileURL string `json:"source_file_url"`
+
+	Pubcli *pub.Service
 	Env api.Env
 }
 
 func (p *Pub) Init(conf, env, path string) (err error) {
 
+	if err = config.LoadEx(p, conf); err != nil {
+		err = errors.Info(err, "pub_image load conf failed", conf)
+		return
+	}
+	if err = config.LoadEx(&p.Env, env); err != nil {
+		err = errors.Info(err, "pub_image load env failed", env)
+		return
+	}
+	dt := da.NewTransport(p.Env.AccessKey, p.Env.SecretKey, nil)
+	p.Pubcli, err = pub.New(p.Env.Hosts["pub"], p.Env.Ips["pub"], dt)
+	if err != nil {
+		err = errors.Info(err, "pub_image init failed")
+		return
+	}
+	return
 }
+
+func (p *Pub) doTestImage() (msg string, err error) {
+
+	code, err := p.Pubcli.Image(p.Bucket, )
+}
+
 
 func (p *Pub) Test() (msg string, err error) {
 
